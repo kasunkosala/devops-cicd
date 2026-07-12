348bb5efc31cb697181324f03ba85b136570da4b
Dr. Kasun Jinasena <kasunkosala@yahoo.com>
2026-07-12 15:19:02 +0530

    Update App.java
    Removed curly brackets

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -5,11 +5,9 @@ public class App {
         System.out.println("Hello, Maven 3!");
 
         for (int i=0; i<10; i++)
-            {
                 if (i%2==0)
                     System.out.println("Even");
                 else
                     System.out.println("Odd");
-            }
     }
 }
