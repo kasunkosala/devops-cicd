2bfcf06ae3f3b630e96a84c364c10abac705a5cd
Dr. Kasun Jinasena <kasunkosala@yahoo.com>
2026-07-12 15:17:50 +0530

    Update App.java
    Added a for loop

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,5 +3,13 @@ package com.example;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello, Maven 3!");
+
+        for (int i=0; i<10; i++)
+            {
+                if (i%2==0)
+                    System.out.println("Even");
+                else
+                    System.out.println("Odd");
+            }
     }
 }
